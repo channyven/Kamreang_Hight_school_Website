59a661e755a0ec238dd0b0f33edae600eeb4445e
Ven Channy <piseyfc088@gmail.com>
2026-07-09 12:39:07 +0700

    delete folder

--- kamrieng-highschool/src/components/admin/VisitorChartWrapper.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import dynamic from "next/dynamic";
-
-const VisitorChart = dynamic(() => import("./VisitorChart"), { ssr: false });
-
-export default function VisitorChartWrapper() {
-  return <VisitorChart />;
-}
